5f870f547d19b5e14c02a6d02f02f760c5d743c2
Balaprasad133 <balaprasadh1332gmail.com>
2024-02-22 11:08:55 +0530

    first comt

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
+
+/*NOTES: The search from BooksAPI is limited to a particular set of search terms.
+You can find these search terms here:
+https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+However, remember that the BooksAPI.search method DOES search by title or author. 
+So, don't worry if you don't find a specific author or title. Every search is limited by search terms.*/
